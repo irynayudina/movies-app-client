627c5fa2fbc7aa5f7e343466b6ee1bf32d46d110
Iryna Yudina <yudinaira4444@gmail.com>
2022-10-15 00:42:56 +0300

    some debugs and code split

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,50 +1,36 @@
 import axios from 'axios';
 import newContext from '../contexts/newContext';
+import React from 'react';
 import { useContext, useEffect, useState } from 'react';
+import Review from '../elements/Review';
 const Reviews = () =>{
     const messageFromContextProvidedbyParentFilm = useContext(newContext)
     const [user, setUser] = useState("")
     useEffect(()=>{
         setUser(JSON.parse(localStorage.getItem('user')))
         console.log(localStorage.getItem('user'))
-        console.log("messageFromContextProvidedbyParentFilm logged in child reviews" + messageFromContextProvidedbyParentFilm)
+        console.log("messageFromContextProvidedbyParentFilm logged in child reviews " + messageFromContextProvidedbyParentFilm)
     }, [])
 
     const [textValid, setTextValid] = useState(false)
-    const [textValid2, setTextValid2] = useState(false)
 
     const [textInp, setTextInp] = useState("")
-    const [textInp2, setTextInp2] = useState("")
     const [ratingInp, setRatingInp] = useState(8.9)
-    const [ratingInp2, setRatingInp2] = useState(8.9)
 
     const [textInpTouched, setTextInpTouched] = useState(false)
-    const [textInpTouched2, setTextInpTouched2] = useState(false)
 
     const [review, setReview] = useState("")
-    const [review2, setReview2] = useState("")
     const [reviews, setReviews] = useState([])
 
     const textHandler = (e)=>{
     setTextInp(e.target.value)
     }
-    const textHandler2 = (e)=>{
-    setTextInp2(e.target.value)
-    }
     const ratingHandler = (e) =>{
         setRatingInp(e.target.value)
     }
-    const ratingHandler2 = (e) =>{
-        setRatingInp2(e.target.value)
-    }
-
     const textBlurHandler = (e) => {
     setTextInpTouched(true);
     setTextValid((textInp !== "") || !textInpTouched);
-    }
-    const textBlurHandler2 = (e) => {
-    setTextInpTouched2(true);
-    setTextValid2((textInp2 !== "") || !textInpTouched2);
     }
     const addReview = (e) => {
         e.preventDefault();
@@ -65,8 +51,8 @@ const Reviews = () =>{
                 let o = res.data
                 if(!o.error){
                     setReview(o)
-                    console.log(o)
-                    // window.location.replace(window.location)
+                    setTextInp("")
+                    setRatingInp(8.9)
                 }
                 })
                 .catch((err) => {
@@ -91,51 +77,7 @@ const Reviews = () =>{
         .catch((err) => {
         console.log(err)
         });
-    }, [review, review2])
-    const removeComment = (cid) =>{
-        axios.post('https://movies-catalog-app.herokuapp.com/user/review/del/full', {
-            rwid: cid
-        })
-        .then((res) => {
-        let o = res.data
-        if(!o.error){
-            setReview(o)
-        }
-        })
-        .catch((err) => {
-        console.log(err)
-        });
-    }
-    const [answerForm, setAFVisible] = useState(false)
-    const showEdditCommentForm = (ct, crat) => {
-        setAFVisible((prevState) => !prevState)
-        setRatingInp2(crat)
-        setTextInp2(ct)
-    }
-    const editReview = (e, cid) => {
-        e.preventDefault();
-        if(textValid2){
-            console.log("edit")
-            console.log("text" + textInp2 + " rating " + ratingInp2 + " review ID " + cid)
-            axios.post('https://movies-catalog-app.herokuapp.com/user/review/update', {
-            text: textInp2,
-            rating: ratingInp2,
-            rwid: cid
-            })
-            .then((res) => {
-            let o = res.data
-            console.log("response after editing")
-            console.log(res)
-            if(!o.error){
-                setReview2(o)
-            }
-            })
-            .catch((err) => {
-            console.log(err)
-            });
-        }
-        setAFVisible(false);
-    }
+    }, [review])
     return  (
         <>
         <div className="add-review"> 
@@ -143,12 +85,13 @@ const Reviews = () =>{
         <div className="hr"></div>
         <form onSubmit={addReview}>
             <label htmlFor="text">Text</label>
-            <textarea name="text" className="form-control" id="text" cols="30" rows="10" 
+            <textarea onClick={()=>{if(!user?._id){
+                alert("Log in before adding review")
+            }}} name="text" className="form-control" id="text" cols="30" rows="10" 
             onChange={textHandler}
             onBlur={textBlurHandler}
             value={textInp} />
-            {textInpTouched && !textValid && <div className='err'><small>Text can`t be empty</small></div>}
-            
+            {textInpTouched && !textValid && <div className='err'><small>Text can`t be empty</small></div>}            
             <label htmlFor="user-password-login">Rating</label>
             <input type="number" max="10" step="0.1" min="1" className="form-control" 
             id="rating" name="rating" aria-describedby="directorsHelp" placeholder="8.9"
@@ -158,34 +101,9 @@ const Reviews = () =>{
         </form>
         </div>  
         {reviews.map((r) => (
-        <div className='comment' key={r?._id}>
-            {r?.user == user?._id ? <div className='delete-comment' onClick={()=>removeComment(r?._id)}>delete</div>: ""}
-            {r?.user == user?._id ? <div className='delete-comment' onClick={()=>showEdditCommentForm(r.text, r.rating)}>edit</div>: ""}
-            {answerForm ? <form onSubmit={(e)=>editReview(e, r._id)}>
-            <label htmlFor="text">Text</label>
-            <textarea name="textEdit" className="form-control" id="textEdit" cols="30" rows="10" 
-            onChange={textHandler2}
-            onBlur={textBlurHandler2}
-            value={textInp2} />
-            {textInpTouched2 && !textValid2 && <div className='err'><small>Text can`t be empty</small></div>}            
-            <label htmlFor="user-password-login">Rating</label>
-            <input type="number" max="10" step="0.1" min="1" className="form-control" 
-            id="ratingEdit" name="ratingEdit" aria-describedby="directorsHelp" placeholder="8.9"
-            onChange={ratingHandler2}
-            value={ratingInp2} />
-            <button type="submit" >Submit</button>
-            </form> : ""}
-            <div className='comment-username'><a href={r?.user == user?._id ? `https://movies-app-playlists.netlify.app/user/${r?.user}/playlists` : `https://movies-app-playlists.netlify.app/user/${r?.user}/playlists/public?uid=${r?.user}&uname=${r?.username}`}>
-                {r?.username}
-            </a></div>
-            {!answerForm ? <>
-            <p className='err'>{r.rating}</p>
-            <p>{r.text}</p></> : ""}
-            {r.isUpdated ? <small className='reviewUpdated'>Updated {r.updatedAt?.slice(0,10)}, {r.updatedAt.slice(12, 16)}</small> : ""}
-            {r.isUpdated ? <p className='date-review'><small>{r.updatedAt?.slice(0,10)}</small></p> : 
-            <p className='date-review'><small>{r.createdAt.slice(0,10)}</small></p>}
-        </div>
-      ))} 
+            <React.Fragment  key={r._id}>
+                <Review rev={r} user={user}/>
+            </React.Fragment>))} 
         </>
     )
 }
---
+++ src/elements/Review.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+const Review = (props) =>{
+    const user = props.user
+    const [editForm, setEditFormVisible] = useState(false)
+    const [reviewEdited, setReviewEdited] = useState(props.rev)
+    const r = reviewEdited;
+    const [reviewRemoved, setReviewRemoved] = useState("")
+    const [textInpEdited, settextInpEdited] = useState("")
+    const [ratingInpEdited, setratingInpEdited] = useState(8.9)
+    const [textInpTouchedEdited, settextInpTouchedEdited] = useState(false)
+    const [textValidEdited, settextValidEdited] = useState(false)
+    const removeComment = (cid) =>{
+        axios.post('https://movies-catalog-app.herokuapp.com/user/review/del/full', {
+            rwid: cid
+        })
+        .then((res) => {
+        let o = res.data
+        if(!o.error){
+            setReviewRemoved(o)
+        }
+        })
+        .catch((err) => {
+        console.log(err)
+        });
+    }
+    const textHandlerEdited = (e)=>{
+        settextInpEdited(e.target.value)
+    }
+    const ratingHandlerEdited = (e) =>{
+        setratingInpEdited(e.target.value)
+    }
+    const textBlurHandlerEdited = (e) => {
+        settextInpTouchedEdited(true);
+        settextValidEdited((textInpEdited !== "") || !textInpTouchedEdited);
+    }
+    const showEdditCommentForm = (ct, crat) => {
+        setEditFormVisible((prevState) => !prevState)
+        setratingInpEdited(crat)
+        settextInpEdited(ct)
+    }
+    const editReview = (e, cid) => {
+        e.preventDefault();
+        if(textValidEdited){
+            console.log("edit")
+            console.log("text" + textInpEdited + " rating " + ratingInpEdited + " review ID " + cid)
+            axios.post('https://movies-catalog-app.herokuapp.com/user/review/update', {
+            text: textInpEdited,
+            rating: ratingInpEdited,
+            rwid: cid
+            })
+            .then((res) => {
+            let o = res.data
+            console.log("response after editing")
+            console.log(res)
+            if(!o.error){
+                setReviewEdited(o)
+            }
+            })
+            .catch((err) => {
+            console.log(err)
+            });
+        }
+        setEditFormVisible(false);
+    }
+    return(
+        <>
+        {!reviewRemoved && <div className='comment'>
+            {r?.user == user?._id ? <div className='delete-comment' onClick={()=>removeComment(r?._id)}>delete</div>: ""}
+            {r?.user == user?._id ? <div className='delete-comment' onClick={()=>showEdditCommentForm(r.text, r.rating)}>edit</div>: ""}
+            <div className='delete-comment' onClick={()=>{
+                if(!user?._id){
+                    alert("Log in before adding review")
+                }
+            }}>answer</div>
+            {editForm ? <form onSubmit={(e)=>editReview(e, r._id)}>
+                <label htmlFor="text">Text</label>
+                <textarea name="textEdit" className="form-control" id="textEdit" cols="30" rows="10" 
+                onChange={textHandlerEdited}
+                onBlur={textBlurHandlerEdited}
+                value={textInpEdited} />
+                {textInpTouchedEdited && !textValidEdited && <div className='err'><small>Text can`t be empty</small></div>}            
+                <label htmlFor="user-password-login">Rating </label>
+                <input type="number" max="10" step="0.1" min="1" className="form-control" 
+                id="ratingEdit" name="ratingEdit" aria-describedby="directorsHelp" placeholder="8.9"
+                onChange={ratingHandlerEdited}
+                value={ratingInpEdited} /> 
+                <button type="submit" className='submit-edited-review'>Submit</button> <label className='clickable-text' onClick={()=>{setEditFormVisible(false)}}><small>Cancel</small></label>
+                </form> : ""
+            }
+            <div className='comment-username'><a href={r?.user == user?._id ? `http://localhost:3000/user/${r?.user}/playlists` : `http://localhost:3000/user/${r?.user}/playlists/public?uid=${r?.user}&uname=${r?.username}`}>
+                {r?.username}
+            </a></div>
+            {!editForm ? <>
+            <p className='err'>{r.rating}</p>
+            <p>{r.text}</p></> : ""}
+            {r.isUpdated ? <small className='reviewUpdated'>Updated {r.updatedAt?.slice(0,10)}, {r.updatedAt?.slice(11, 16)}</small> : ""}
+            <p className='date-review'><small>{r.createdAt?.slice(0,10)}</small></p>    
+        </div>}        
+        </>
+    )
+}
+export default Review;
